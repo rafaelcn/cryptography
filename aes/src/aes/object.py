36bc8db51c669aa62e5027b113cfb3c7dc208dcf
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-09-06 11:37:03 -0300

    Add the AES object
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ aes/src/aes/object.py
@@ -0,0 +1,11 @@
+
+
+class AES:
+    def __init__(self, mode) -> None:
+        pass
+
+    def encrypt(self, plaintext: bytes, key: bytes) -> bytes:
+        pass
+
+    def decrypt(self, ciphertext: bytes, key: bytes) -> bytes:
+        pass
