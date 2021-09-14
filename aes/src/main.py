8db379b3ecb5dac6c42139df6c472cda892641c3
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-09-14 18:57:53 -0300

    Add (buggy) implementation of CTR
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- aes/src/main.py
+++ aes/src/main.py
@@ -1,5 +1,8 @@
+import os
 import math
 import argparse
+
+from aes import ecb, ctr
 
 
 def create_parser():
@@ -8,8 +11,8 @@ def create_parser():
     p.add_argument('-k', '--key', help='key', required=True)
     p.add_argument('-i', '--input', help='input file', required=True)
     p.add_argument('-o', '--output', help='output file', required=False)
-    p.add_argument('-s', '--block-size', help='block size', default=128,
-                   type=int, required=False)
+    p.add_argument('-s', '--block-size', help='block size in bytes',
+                   default=16, type=int, required=False)
     p.add_argument('-m', '--modes', help='AES modes of operation',
                    default='ecb', choices=['ecb', 'ctr'], required=False)
 
@@ -25,85 +28,53 @@ def main():
 
     data = ""
 
-    # print("parser.key: ",parser.key)
-    # print("parser.key.len: ",len(parser.key))
-    print(parser.block_size)
-    print(parser.modes)
-    print(parser.input)
+    print("{}".format(parser.block_size))
+    print("{}".format(parser.modes))
+    print("{}".format(parser.input))
 
     with open(parser.input, 'rb') as f:
         data = f.read()
 
     key_size = len(parser.key)
-    print("key_size: ", key_size)
-    block_size = math.ceil(len(data) / parser.block_size)
+    data_block_size = math.ceil(len(data) / parser.block_size)
 
-    print('image with {} bytes, creating {} blocks...'.format(len(data),
-          block_size))
-    print('key with {} bytes'.format(key_size))
+    print('image with {} bytes, creating {} blocks'.format(len(data),
+          data_block_size))
+    print('key with {} bytes, filling with zeros'.format(key_size))
 
     key_block = bytes(parser.key, 'utf-8')
-    #print("key_block_len:",len(key_block))
 
     # fill the key with zeros
     if key_size < parser.block_size:
         quantity = parser.block_size - key_size
-        #print("quantity", quantity)
         key_block += b'\x00' * quantity
 
     # fill the data with zeros if it's not a multiple of the block size
     if len(data) % parser.block_size != 0:
-      quantity = parser.block_size - (len(data) % parser.block_size)
-      data += b'\x00' * quantity
-    print(len(key_block))
-    print(len(data))
-    # for i, k in data_blocks.items():
-    #   print("block {}".format(i), k, "\n\n")
-    #
-    # print("key", key_block)
-    # print(len(key_block))
+        quantity = parser.block_size - (len(data) % parser.block_size)
+        data += b'\x00' * quantity
+
     if parser.modes == 'ecb':
-        import aes_ecb
-        key = key_block
-        encrypted = aes_ecb.AES(key).encrypt_block(data)
-        fin = open('github_profile_copy.png', 'wb')
-        # writing encrypted data in image
-        fin.write(encrypted)
-        fin.close()
-        print('Encryption Done...')
-        decrypted = aes_ecb.AES(key).decrypt_block(encrypted)
-        fin = open('github_profile_copy.png', 'wb')
-        
-        # writing decryption data in image
-        fin.write(decrypted)
-        fin.close()
-        print('Decryption Done...')
-    if parser.modes == 'ctr':
-        # ctr mode
-        import aes_ctr
-        import os
-        key = key_block
-        
-        iv = os.urandom(128)
-        # print(iv)
-        encrypted = aes_ctr.AES(key).encrypt_ctr(data, iv)
-        # converting image into byte array to
-        # perform encryption easily on numeric data
-        fin = open('github_profile_copy.png', 'wb')
-        # writing encrypted data in image
-        fin.write(encrypted)
-        fin.close()
-        print('Encryption Done...')
-        #encrypted = aes.AES(key).encrypt_ctr(, iv)
-        #print(encrypted)
-        print(len(encrypted))
-        decrypted = aes_ctr.AES(key).decrypt_ctr(encrypted, iv)
-        fin = open('github_profile_copy.png', 'wb')
-        
-        # writing decryption data in image
-        fin.write(decrypted)
-        fin.close()
-        print('Decryption Done...')
+        alg = ecb.ECB(key_block)
+
+        cryptogram = alg.encrypt(data)
+
+        with open(parser.output, 'wb') as f:
+            f.write(cryptogram)
+
+        with open(parser.output+".png", 'wb') as f:
+            f.write(alg.decrypt(cryptogram))
+
+    elif parser.modes == 'ctr':
+        alg = ctr.CTR(key_block, os.urandom(128))
+
+        cryptogram = alg.encrypt(key_block)
+
+        with open(parser.output, 'wb') as f:
+            f.write(cryptogram)
+
+        with open(parser.output+".png", 'wb') as f:
+            f.write(alg.decrypt(cryptogram))
 
 
 if __name__ == "__main__":
