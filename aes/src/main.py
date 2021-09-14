4a06e867ccbee5fb5910ba2e1b25e7d2ff9f6285
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-09-13 22:29:47 -0300

    Add code for block segmentation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- aes/src/main.py
+++ aes/src/main.py
@@ -39,7 +39,31 @@ def main():
     print('image with {} bytes, creating {} blocks...'.format(len(data),
           block_size))
     print('key with {} bytes'.format(key_size))
-    #print(data)
+
+    data_blocks = {}
+    key_block = bytes(parser.key, 'utf-8')
+
+    for i in range(block_size):
+        data_blocks[i] = [data[i * parser.block_size:(i + 1) * parser.block_size]]
+
+    # fill the last block with zeros
+    if len(data) % parser.block_size != 0:
+        quantity = parser.block_size - len(data_blocks[block_size - 1][0])
+        data_blocks[block_size - 1].append(b'\x00' * quantity)
+
+    # fill the key with zeros
+    if key_size < parser.block_size:
+        quantity = parser.block_size - key_size
+        key_block += b'\x00' * quantity
+
+    # for i, k in data_blocks.items():
+    #    print("block {}".format(i), k, "\n\n")
+    #
+    # print("key", key_block)
+
+    if parser.modes == 'ecb':
+        from aes import cypher
+        cypher.encrypt(data_blocks, key_block, data_blocks)
 
 
 if __name__ == "__main__":
