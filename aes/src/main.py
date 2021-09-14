290908ec24b47979b57ea3ab65f9e6cbd146e049
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-09-14 01:13:16 -0300

    Make the data a multiple of the block size
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- aes/src/main.py
+++ aes/src/main.py
@@ -40,21 +40,17 @@ def main():
           block_size))
     print('key with {} bytes'.format(key_size))
 
-    data_blocks = {}
     key_block = bytes(parser.key, 'utf-8')
-
-    for i in range(block_size):
-        data_blocks[i] = [data[i * parser.block_size:(i + 1) * parser.block_size]]
-
-    # fill the last block with zeros
-    if len(data) % parser.block_size != 0:
-        quantity = parser.block_size - len(data_blocks[block_size - 1][0])
-        data_blocks[block_size - 1].append(b'\x00' * quantity)
 
     # fill the key with zeros
     if key_size < parser.block_size:
         quantity = parser.block_size - key_size
         key_block += b'\x00' * quantity
+
+    # fill the data with zeros if it's not a multiple of the block size
+    if len(data) % parser.block_size != 0:
+        quantity = parser.block_size - (len(data) % parser.block_size)
+        data += b'\x00' * quantity
 
     # for i, k in data_blocks.items():
     #    print("block {}".format(i), k, "\n\n")
@@ -63,7 +59,8 @@ def main():
 
     if parser.modes == 'ecb':
         from aes import cypher
-        cypher.encrypt(data_blocks, key_block)
+        #cypher.encrypt(data_blocks, key_block)
+        cypher.encrypt(bytearray(data), key_block)
 
 
 if __name__ == "__main__":
