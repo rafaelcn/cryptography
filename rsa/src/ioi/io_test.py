a6eb81f005b38e87db67f096fb577d51a4056843
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-10-27 22:35:41 -0300

    Fix test runtim
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- rsa/src/ioi/io_test.py
+++ rsa/src/ioi/io_test.py
@@ -3,21 +3,21 @@ import tempfile
 
 from ioi import io
 
+
 class TestKeyAlgorithms(unittest.TestCase):
     def test_read_key(self):
-        # write a temp key in the temp folder of the OS and read it
         key_1 = {'n': 1024, 'e': 3239817237812367123718232}
 
-        with tempfile.TemporaryFile(mode='r+') as file:
-            file.write(f'{key_1}')
-            file.seek(0)
+        file = tempfile.TemporaryFile(mode='r+')
 
-            data = io.read_key(file.name)
+        file.write(f'{key_1}')
+        file.seek(0)
 
-            self.assertEqual(data, key_1)
-    
+        data = io.read_key(file.name)
+
+        # TODO: fix error of file not being closed...
+        self.assertEqual(data, key_1)
 
 
 if __name__ == "__main__":
     unittest.main()
-
