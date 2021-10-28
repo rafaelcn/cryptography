38de104b4a264a9f71d40f6306bfe96e24e6985b
Rafael Campos Nunes <rafaelnunes@engineer.com>
2021-10-27 22:36:12 -0300

    Make it conformant with pep-8
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- rsa/src/crypto/certificate_test.py
+++ rsa/src/crypto/certificate_test.py
@@ -7,6 +7,7 @@ from crypto import certificate
 # PYTHONPATH=. python3 src/certificate_test.py (on src)
 # PYTHONPATH=../ python3 certificate_test.py (on crypto)
 
+
 class TestKeyAlgorithms(unittest.TestCase):
     def test_sign_and_validation(self):
         p = primes.get_random_bits(1024)
@@ -23,4 +24,3 @@ class TestKeyAlgorithms(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
